0c27767d73c726bf80030c10340ff441a5f25ae5
ramanagali <ramana.gali@gmail.com>
2022-02-28 19:25:09 +0800

    docker build

---
+++ src/server.js
@@ -0,0 +1,15 @@
+'use strict';
+const express = require('express');
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (_, res) => {
+  res.send({
+    message: "Hello World NodeJs!",
+  });
+});
+
+const server = app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
