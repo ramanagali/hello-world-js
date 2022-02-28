1ac8cd82d1f1170e4f23d73af0b4a83631417b38
ramanagali <ramana.gali@gmail.com>
2022-03-01 00:01:37 +0800

    docker build 1.0.0

--- src/server.js
+++ src/server.js
@@ -5,7 +5,10 @@ const HOST = 'localhost';
 
 const app = express();
 app.get('/', (_, res) => {
-  res.send({ message: "Hello World NodeJs", });
+  res.send({
+    message: "Hello World NodeJs",
+    version: process.env.npm_package_version,
+  });
 });
 const server = app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
