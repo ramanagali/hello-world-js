71b0dcd89329c09d6bc4ee937e79e2fa187ae1d4
ramanagali <ramana.gali@gmail.com>
2022-03-11 17:53:46 +0800

    release 1.0.3

--- src/server.js
+++ src/server.js
@@ -5,8 +5,10 @@ const PORT = 3000;
 const app = express();
 app.get('/', (_, res) => {
   res.send({
-    message: "Hello World NodeJs",
+    app: process.env.npm_package_name,
     version: process.env.npm_package_version,
+    message: "Hello World NodeJs",
+    author: process.env.npm_package_author_name
   });
 });
 const server = app.listen(PORT, () => {
